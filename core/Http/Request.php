93e1c5cfcfacd54daab3a482983ad48cf99b16aa
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-08 17:49:11 +0200

    RequestClass

---
+++ core/Http/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core\Http;
+
+class Request
+{
+    public function __construct(
+        
+    )
+    {
+    }
+
+    public static function createFromGlobals(): static
+    {
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
+//Front Controller
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
+$request = Request::createFromGlobals();
 
 dd("test");
