3d4a0209a09c93c2cc57173699b4feea1dd20142
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-09 15:38:01 +0200

    KERNEL HANDLE ROUTER

--- core/Http/Kernel.php
+++ core/Http/Kernel.php
@@ -2,10 +2,24 @@
 
 namespace Core\Http;
 
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        return new Response(content: '<h1>Hello</h1>', status: 404, headers: []);
+        $dispatcher = simpleDispatcher(function(RouteCollector $router) {
+            $router->addRoute('GET', '/', function(){
+                return new Response(content: '<h1>Hi!</h1>');
+            });
+        });
+        
+        $routerInfo = $dispatcher->dispatch($request->server['REQUEST_METHOD'], $request->server['REQUEST_URI']);
+
+        // [$status, $handler, $vars] = $routerInfo;
+
+        return $routerInfo[1]($routerInfo[2]);
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,9 +3,6 @@
 use Core\Http\Kernel;
 use Core\Http\Request;
 use Core\Http\Response;
-use FastRoute\RouteCollector;
-
-use function FastRoute\simpleDispatcher;
 
 //Front Controller
 
@@ -15,21 +12,11 @@ $request = Request::createFromGlobals();
 
 $kernel = new Kernel();
 
-$dispatcher = simpleDispatcher(function(RouteCollector $router) {
-    $router->addRoute('GET', '/', function(){
-        return new Response(content: '<h1>Hi!</h1>');
-    });
-});
+$response = $kernel->handle($request);
 
-dd($request);
+$response->send();
 
-$routerInfo = $dispatcher->dispatch();
-
-//$response = $kernel->handle($request);
-
-//$response->send();
-
-//dd($response);
+dd($response);
 
 // !@ everytime you add new library/package, you must index workspace !@ 
 // View => command palette || right click and at the bottom "command palette"
