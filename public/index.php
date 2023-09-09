d80ed258842779b9613397179f789ccdb929e58b
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-09 14:43:21 +0200

    request class

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php declare(strict_types=1);
+
+use Core\Http\Request;
+
 //Front Controller
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 $request = Request::createFromGlobals();
 
-dd("test");
+dd($request);
