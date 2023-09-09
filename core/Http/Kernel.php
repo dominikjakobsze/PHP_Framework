649f6c5f7aa628480e39419e73d8a59bef7ef0c7
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-09 15:45:38 +0200

    kernel handle

--- core/Http/Kernel.php
+++ core/Http/Kernel.php
@@ -11,14 +11,20 @@ class Kernel
     public function handle(Request $request): Response
     {
         $dispatcher = simpleDispatcher(function(RouteCollector $router) {
+
             $router->addRoute('GET', '/', function(){
                 return new Response(content: '<h1>Hi!</h1>');
             });
+
+            $router->addRoute('GET', '/posts/{id:\d+}', function($routeParams){
+                return new Response(content: "<h1>This is post {$routeParams['id']} </h1>");
+            });
+
         });
         
         $routerInfo = $dispatcher->dispatch($request->server['REQUEST_METHOD'], $request->server['REQUEST_URI']);
 
-        // [$status, $handler, $vars] = $routerInfo;
+        // [$status, $handler, $routeParams] = $routerInfo;
 
         return $routerInfo[1]($routerInfo[2]);
     }
