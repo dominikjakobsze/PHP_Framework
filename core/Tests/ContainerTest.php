130c8323e1b75c3096955e7f142871d96b5f59cd
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-22 19:46:38 +0200

    phpunit added

---
+++ core/Tests/ContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
