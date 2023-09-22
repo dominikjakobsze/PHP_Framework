6ea1160f2a933849d3317e4e9b54bcc6440bb17a
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-22 19:51:28 +0200

    phpunit configured

--- core/Tests/ContainerTest.php
+++ core/Tests/ContainerTest.php
@@ -6,8 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
 {
-    public function testItWorks()
+    public function test_a_service_can_be_retrieved_from_the_container()
     {
         $this->assertTrue(true);
     }
 }
+
+// run tests => vendor/bin/phpunit core/Tests --colors
