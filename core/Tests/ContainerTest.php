5d7862dff656a949c681d821b14e3d3be1326293
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-09-23 18:11:11 +0200

    composer new dep

--- core/Tests/ContainerTest.php
+++ core/Tests/ContainerTest.php
@@ -26,6 +26,22 @@ class ContainerTest extends TestCase
         $container->add("dependant-class");
     }
 
+    public function test_exception_is_not_thrown_a_service_can_be_found_by_string()
+    {
+        $container = new Container();
+
+        $container->add("Core\Tests\DependantClass");
+
+        $this->assertTrue($container->has("Core\Tests\DependantClass"));
+
+        $container->add("dependant-class", "Core\Tests\DependantClass");
+
+        $this->assertTrue($container->has("dependant-class"));
+
+        $this->assertInstanceOf(DependantClass::class, $container->get("dependant-class"));
+        $this->assertInstanceOf(DependantClass::class, $container->get("Core\Tests\DependantClass"));
+    }
+
     public function test_a_container_has_a_service()
     {
         $container = new Container();
@@ -51,4 +67,8 @@ class DependantClass
     }
 }
 
+
 // run tests => vendor/bin/phpunit core/Tests --colors
+
+// run single specific test
+// vendor/bin/phpunit core/Tests/ContainerTest.php --colors --filter test_exception_is_not_thrown_a_service_can_be_found_by_string
